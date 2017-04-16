0840371465fbfb5b6bbe3231aed2876a57fc5248
caolei.cl <caolei.cl@alibaba-inc.com>
2017-04-16 22:01:27 +0800

    add spark and scala dependency

---
+++ src/main/java/org/hackx/streaming/HelloWorld.scala
@@ -0,0 +1,12 @@
+package org.hackx.streaming
+
+object HelloWorld {
+
+    def main(args: Array[String]) {
+
+        for (i <- 1 to 10) {
+            println(i)
+        }
+    }
+    
+}
---
+++ src/main/java/org/hackx/streaming/SparkStreamingDemo.java
@@ -0,0 +1,46 @@
+package org.hackx.streaming;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class SparkStreamingDemo {
+    private static final Pattern SPACE = Pattern.compile(" ");
+
+    public static void main(String[] args) throws Exception {
+
+        SparkConf sparkConf = new SparkConf().setMaster("local[2]").setAppName("JavaNetworkWordCount");
+        JavaStreamingContext javaStreamingContext = new JavaStreamingContext(sparkConf, Durations.seconds(1L));
+        javaStreamingContext.checkpoint("/Users/caolei/WorkSpace/hack-child/src/main/resources/checkpoint");
+
+
+        List<Tuple2<String, Integer>> tuples = Arrays.asList(new Tuple2<>("hello", 1), new Tuple2<>("world", 1));
+        JavaPairRDD<String, Integer> initialRDD = javaStreamingContext.sparkContext().parallelizePairs(tuples);
+
+        JavaReceiverInputDStream<String> lines = javaStreamingContext.socketTextStream("localhost", 9999);
+        JavaDStream<String> words = lines.flatMap(line -> Arrays.asList(SPACE.split(line)).iterator());
+        JavaPairDStream<String, Integer> wordPairs = words.mapToPair(word -> new Tuple2<>(word, 1));
+
+        Function2<Integer, Integer, Integer> reduceFunc = (Integer x1, Integer x2) -> x1 + x2;
+
+        // Reduce last 30 seconds of data, every 10 seconds
+        JavaPairDStream<String, Integer> windowedWordCounts = wordPairs.reduceByKeyAndWindow(reduceFunc, Durations.seconds(8), Durations.seconds(4));
+        windowedWordCounts.print();
+
+        /*JavaPairDStream<String, Integer> wordCounts = wordPairs.reduceByKey((x, y) -> x + y);
+        wordCounts.print();*/
+
+        javaStreamingContext.start();
+        javaStreamingContext.awaitTermination();
+    }
+}
